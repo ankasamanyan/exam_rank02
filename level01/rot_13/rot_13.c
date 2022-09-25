d703def45cb6f8fe9e0f8784a6ea5aab952f2e62
Anahit Kasamanyan <anikakasamanyan@gmail.com>
2022-09-25 18:16:31 +0200

    halp

--- level01/rot_13/rot_13.c
+++ level01/rot_13/rot_13.c
@@ -3,6 +3,34 @@
 
 int main(int argc, char const *argv[])
 {
+	int i = 0;
+	int j;
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return 0;
+	}
+	while (argv[1][i])
+	{
+		if ((argv[1][i] >= 'a' && argv[1][i] <= 'z'))
+		{
+			j = argv[1][i] + 13;
+			if (j > 'z')
+				j = j - 26;
+			write(1, &j, 1);
+		}
+		else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+		{
+			j = argv[1][i] + 13;
+			if (j > 'Z')
+				j = j - 26;
+			write(1, &j, 1);
+		}
+		else
+			write(1, &argv[1][i], 1);
+		i++;
+	}
+		write(1, "\n", 1);
 	
 	return 0;
 }

940f2e91e46a5e3fa749cc1fb2bc0a24c0c81f5b
Anahit K <anikakasamanyan@gmail.com>
2022-09-26 12:42:05 +0200

    halp

--- level02/do_op/do_op.c
+++ level02/do_op/do_op.c
@@ -4,10 +4,31 @@
 
 int main( int argc, char **argv)
 {
-    int i = 0;
+	int res = 0;
     if (argc != 4)
     {
         write(1, "\n",1);
         return 0;
     }
+	if (argv[2][0] == '*')
+	{
+		res = atoi(argv[1]) * atoi(argv[3]);
+		printf("%i", res);
+	}
+	else if (argv[2][0] == '/')
+	{
+		res = atoi(argv[1]) / atoi(argv[3]);
+		printf("%i", res);
+	}
+	else if (argv[2][0] == '+')
+	{
+		res = atoi(argv[1]) + atoi(argv[3]);
+		printf("%i", res);
+	}
+	else if (argv[2][0] == '-')
+	{
+		res = atoi(argv[1]) - atoi(argv[3]);
+		printf("%i", res);
+	}
+	return 0;
 }
---
+++ level02/ft_atoi/ft_atoi.c
@@ -0,0 +1,12 @@
+#include "unistd.h"
+
+int	ft_atoi(const char *str)
+{
+	int number = 0;
+	int sign = -1;
+	int i = 0;
+	while (str[i])
+	{
+		
+	}
+}

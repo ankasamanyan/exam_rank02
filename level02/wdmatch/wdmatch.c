feb62039d33ad7f71bd88f6a4e36a40fe3453c96
Anahit Kasamanyan <anikakasamanyan@gmail.com>
2022-09-30 17:40:01 +0200

    halp

--- level02/wdmatch/wdmatch.c
+++ level02/wdmatch/wdmatch.c
@@ -4,9 +4,15 @@ int main(int argc, char **argv)
 {
 	int i = 0;
 	int j = 0;
-	int temp = 0;
 	if (argc != 3)
 		return (write(1, "\n", 1));
-	
+	while (argv[2][j])
+	{
+		if (argv[2][j++] == argv[1][i])
+			i++;
+	}
+	j = 0;
+	while (argv[1][i] == '\0' && argv[1][j])
+		write(1, &argv[1][j++], 1);
 	return (write(1, "\n", 1));
 }

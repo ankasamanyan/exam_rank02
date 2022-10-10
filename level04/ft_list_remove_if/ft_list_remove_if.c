63c81c1286bdb2e42903030428c91cb23defa286
Anahit K <anikakasamanyan@gmail.com>
2022-10-11 01:21:31 +0200

    halp

--- level04/ft_list_remove_if/ft_list_remove_if.c
+++ level04/ft_list_remove_if/ft_list_remove_if.c
@@ -2,13 +2,58 @@
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list *temp = *begin_list;
+	t_list *curr = *begin_list;
+	t_list *prev = 0;
 
-	while (temp != NULL)
+	while (curr != NULL)
 	{
-		if((*cmp)(temp->data, data_ref) == 0)
+		if((*cmp)(curr->data, data_ref) == 0)
 		{
-			//halp
+			if (prev == 0)
+				*begin_list = curr->next;
+			else 
+				prev->next = curr->next;
+			free(curr);
 		}
+		prev = curr;
+		curr = curr->next;
 	}
 }
+
+
+// .................
+
+#include "ft_list.h"
+
+#include <stdlib.h>
+
+
+
+/*///////////////////////////////////////////////////////
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+#include "ft_list.h"
+#include <stdlib.h>
+
+void		ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list		*current;
+	t_list		*previous;
+
+	previous = 0;
+	current = *begin_list;
+	while (current)
+	{
+		if ((cmp)(current->data, data_ref) == 0)
+		{
+			if (previous == 0)
+				*begin_list = current->next;
+			else
+				previous->next = current->next;
+			free(current);
+		}
+		previous = current;
+		current = current->next;
+	}
+}
+
+
